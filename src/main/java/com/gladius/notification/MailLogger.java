53ca0eafcc2226c8f04c2e3c747ffc56eda311a4
HD Haasbroek <u15046657@tuks.co.za>
2017-03-22 21:05:05 +0200

    Update MailLogger.java

--- src/main/java/com/gladius/notification/MailLogger.java
+++ src/main/java/com/gladius/notification/MailLogger.java
@@ -1,8 +1,16 @@
 public class MailLogger{
-
+/**
+*logMailSent function logs every mail item that gets sent out
+*@Param type 
+*/
 	public void logMailSent(type) {
 		
 	}
+/**
+*logMailError function logs mailing errors.
+*@Param  error, the error that occured during the message sending.
+
+*/
 	public  void logMailError(String error) {
 		//Log the errors incase any occur
 	}
