3f2ecf15d28fd598974ea043f6bfa1ca1dbf3c8a
craig95 <u15029779@tuks.co.za>
2017-03-22 22:50:57 +0200

    Changed class name to NotificationHandler

--- src/main/java/com/gladius/notification/NotificationHandler.java
+++ src/main/java/com/gladius/notification/NotificationHandler.java
@@ -1,6 +1,6 @@
 package com.gladius.notification;
 
-public class Notification {
+public class NotificationHandler {
     public static void main(String[] args) {
         Mailer mailer = new Mailer("gladius.notification@gmail.com", "smtp.gmail.com", "587", true, "gladius.notification@gmail.com", "9FM-mZD-wtC-trd");
         System.out.println(mailer.sendNotification("NavUP Notification", "<h1>Hello World</h1>", "crvheerden@gmail.com"));
